93be7f8554781cc50b5e97e8abb15e9190bb80d7
Ahmet Erdonmez <ahmet.erdonmez77@gmail.com>
2024-10-09 23:03:31 +0200

    relevant dependencies installed

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+# Get the long description from the README file
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="py_utils",
+    version="0.0.1",
+    author="Ahmet Erdonmez",
+    author_email="ahmet.erdonmez77@gmail.com",
+    description="Text summarization and text generation with Pytorch and Transformers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ruyisbaros/nlp_transformers_text_summarization",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages("src", exclude=["test"]),
+    classifiers=[
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
